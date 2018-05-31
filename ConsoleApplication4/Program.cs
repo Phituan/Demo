f0d6de336ead5c8429f2c5c8424c6b9b5181fdc6
Phituan <tuandpd00617@gmail.com>
2018-05-31 10:48:16 +0700

    c#

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApplication4.Properties;
+
+namespace ConsoleApplication4
+{
+    internal class Program
+    {
+        private static List<Student> list = new List<Student>();
+
+        private static void Main(string[] args)
+        {
+            GenerateMenu();
+        }
+
+        private static void SearchByName()
+        {
+            Console.WriteLine("please enter name to search: ");
+            var searchKey = Console.ReadLine();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Student student = list[i];
+                if (student.Name == searchKey)
+                {
+                    Console.WriteLine("found");
+                    Console.WriteLine(student.RollNumber + "_" + student.Name);
+                }
+            }
+        }
+
+        private static void AddStudent()
+        {
+            var student = new Student();
+            Console.WriteLine("enter name");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("enter email");
+            student.Email = Console.ReadLine();
+            Console.WriteLine("enter phone");
+            student.Phone = Console.Read();
+            list.Add(student);
+//            Console.WriteLine("do you want to enter ");
+        }
+
+        private static void DisplayStudent()
+        {
+            foreach (var student in list)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+
+        private static void GenerateMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("---------------Student manager-------------");
+                Console.WriteLine("Add new student");
+                Console.WriteLine("Show list student");
+                Console.WriteLine("Search student by name");
+                Console.WriteLine("Exit.");
+                Console.WriteLine("Please enter your choice: ");
+                var choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        AddStudent();
+                        break;
+                    case 2:
+                        DisplayStudent();
+                        break;
+                    case 3:
+                        SearchByName();
+                        break;
+                    case 4:
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication4/Properties/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApplication4.Properties
+{
+    public class Student
+    {
+        private String rollNumber;
+        private String name;
+        private String email;
+        private int phone;
+
+        public Student(string rollNumber,string name, string email, int phone)
+        {
+            this.rollNumber = rollNumber;
+            this.name = name;
+            this.email = email;
+            this.phone = phone;
+        }
+
+        public Student()
+        {
+        }
+
+        public string RollNumber
+        {
+            get { return rollNumber; }
+            set { rollNumber = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.RollNumber + this.name + this.Email + this.Phone ;
+        }
+    }
+}
